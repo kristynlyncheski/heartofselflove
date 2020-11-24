53636628b6c758eb45a479f21453cdc300f28b6c
Kristyn Lyncheski <klyncheski@gmail.com>
2020-11-24 14:35:54 -0500

    adding local script to add .html extension to links

---
+++ js/local.js
@@ -0,0 +1,32 @@
+if (!window.location.hostname || window.location.origin === 'file://') {
+    var links = document.querySelectorAll('a');
+
+    for (var i = 0; i < links.length; i++) {
+        var url = links[i].href;
+        var pattern = /^((http|https|ftp|mailto):)/;
+
+        if (!!url && !pattern.test(url)) {
+            console.log(url)
+            if (url.includes('#')) {
+                var anchor = url.split('#')[1];
+
+                if (url.includes('file:///#')) {
+                    var page = window.location.href;
+                    var string = page.substring(0, page.lastIndexOf("/") + 1);
+
+                    links[i].href = string + "index.html#" + anchor;
+                } else {
+                    links[i].href = url.split('#').join('.html#');
+                }
+
+            } else if (url === 'file:///') {
+                var page = window.location.href;
+                var string = page.substring(0, page.lastIndexOf("/") + 1);
+
+                links[i].href = string + "index.html";
+            } else {
+                links[i].href = url + ".html"
+            }
+        }
+    }
+}
